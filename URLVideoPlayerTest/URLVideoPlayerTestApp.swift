9316233955e290d4a16c8b5f05507ff1440bbd09
Kim JaeHyung <you3192001@icloud.com>
2024-08-16 16:24:48 +0900

    Initial Commit

---
+++ URLVideoPlayerTest/URLVideoPlayerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLVideoPlayerTestApp.swift
+//  URLVideoPlayerTest
+//
+//  Created by Jae hyung Kim on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct URLVideoPlayerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
